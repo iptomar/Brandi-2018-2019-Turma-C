5ee9244aa8455a8557f35abf3b987fed9427aeb7
estt15477 <48072185+estt15477@users.noreply.github.com>
2019-03-27 22:41:12 +0000

    Clone Inicial

---
+++ Testes/Testes Cliente/mavenproject1/src/test/java/Testes_Ficha.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testes_Ficha {
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://brandic.devll.eu:61080/login/login");
+    WebElement email = driver.findElement(By.id("email"));
+    email.sendKeys("teste123456@ipt.pt");
+    WebElement pass = driver.findElement(By.id("password"));
+    pass.sendKeys("123456");
+    driver.findElement(By.id("butLogin")).click();
+  }
+
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @After
+  public void tearDown() throws Exception {}
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
