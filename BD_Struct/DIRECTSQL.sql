24903a955bcfc99280db064dde0c6c9bc1631b7d
aluno20235 <aluno20235@ipt.pt>
2019-04-07 18:55:09 +0100

    Pequenas correções no SQL de criação das tabelas... erros de estrutura

--- BD_Struct/DIRECTSQL.sql
+++ BD_Struct/DIRECTSQL.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS tbl_technics_used(id INT(11) NOT NULL AUTO_INCREMENT,
 
 CREATE TABLE IF NOT EXISTS tbl_supercategories(id INT(11) NOT NULL AUTO_INCREMENT, supercategory VARCHAR(150) NOT NULL, PRIMARY KEY (id));
 
-CREATE TABLE IF NOT EXISTS tbl_categories(id INT(11) NOT NULL AUTO_INCREMENT, category VARCHAR(150) NOT NULL, supercategory INT(11) NOT NULL, PRIMARY KEY (id)), FOREIGN KEY fk_super(supercategory) REFERENCES tbl_supercategories(id);
+CREATE TABLE IF NOT EXISTS tbl_categories (id INT(11) NOT NULL AUTO_INCREMENT, category VARCHAR(150) NOT NULL, supercategory INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY fk_super (supercategory) REFERENCES tbl_supercategories (id));
 
 CREATE TABLE IF NOT EXISTS tbl_subcategories(id INT(11) NOT NULL, subcategory INT(11), category INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY fk_cattop(category) REFERENCES  tbl_categories(id));
 
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS tbl_object(id INT(11) NOT NULL AUTO_INCREMENT, object
 
 CREATE TABLE IF NOT EXISTS tbl_conservation(id INT(11) NOT NULL AUTO_INCREMENT, object_id INT(11) NOT NULL, support_deterioration TEXT, surface_deterioration TEXT, elements_deterioration TEXT, support_diagnostic TEXT, surface_diagnostic TEXT, elements_diagnostic TEXT, conclusions TEXT, PRIMARY KEY (id),FOREIGN KEY fk_conservation_object(object_id) REFERENCES tbl_object(id));
 
-CREATE TABLE IF NOT EXISTS tbl_environment(id INT(11) NOT NULL AUTO_INCREMENT, object_id INT(11) NOT NULL, description TEXT NOT NULL, cold_temp VARCHAR(50), hot_temp VARCHAR(50), cold_humidity VARCHAR(50), hot_humidity VARCHAR(50), cold_start INT(2), cold_end INT(2), hot_start INT(2), hot_end INT(2), lightning_type_natural VARCHAR(50), lightning_origin_artificial VARCHAR(50), artificial_lux VARCHAR(50), natural_lux VARCHAR(50), artificial_uv VARCHAR(50), natural_uv VARCHAR(50), artificial_real_uv VARCHAR(50), natural_real_uv VARCHAR(50), poluting_agents TEXT, poluting_sources TEXT, poluting_results TEXT, conclusions TEXT, PRIMARY KEY (id)),FOREIGN KEY fk_environment_object(object_id) REFERENCES tbl_object(id));
+CREATE TABLE IF NOT EXISTS tbl_environment(id INT(11) NOT NULL AUTO_INCREMENT, object_id INT(11) NOT NULL, description TEXT NOT NULL, cold_temp VARCHAR(50), hot_temp VARCHAR(50), cold_humidity VARCHAR(50), hot_humidity VARCHAR(50), cold_start INT(2), cold_end INT(2), hot_start INT(2), hot_end INT(2), lightning_type_natural VARCHAR(50), lightning_origin_artificial VARCHAR(50), artificial_lux VARCHAR(50), natural_lux VARCHAR(50), artificial_uv VARCHAR(50), natural_uv VARCHAR(50), artificial_real_uv VARCHAR(50), natural_real_uv VARCHAR(50), poluting_agents TEXT, poluting_sources TEXT, poluting_results TEXT, conclusions TEXT, PRIMARY KEY (id),FOREIGN KEY fk_environment_object(object_id) REFERENCES tbl_object(id));
 
 CREATE TABLE IF NOT EXISTS tbl_images(id INT(11) NOT NULL AUTO_INCREMENT, object_id INT(11) NOT NULL, image VARCHAR(170) NOT NULL, main_image BIT(1) NOT NULL, record_type VARCHAR(50), format VARCHAR(50), resolution VARCHAR(20), reference VARCHAR(40), PRIMARY KEY (id),FOREIGN KEY fk_image_object(object_id) REFERENCES tbl_object(id));
 
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS tbl_tests(id INT(11) NOT NULL AUTO_INCREMENT, object_
 
 CREATE TABLE IF NOT EXISTS tbl_analysis(id INT(11) NOT NULL AUTO_INCREMENT, test_id INT(11) NOT NULL, type_reference VARCHAR(150), location VARCHAR(150), objectives VARCHAR(150), technician INT(11) NOT NULL, analysis_DATE DATE, results TEXT, conclusions TEXT, PRIMARY KEY (id), FOREIGN KEY fk_analysis_test(test_id) REFERENCES tbl_tests(id), FOREIGN KEY fk_analysis_tech(technician) REFERENCES tbl_users(id));
 
-CREATE TABLE  IF NOT EXISTS tbl_solubility(id int(11) NOT NULL AUTO_INCREMENT, object int(11) NOT NULL, description varchar(100) NOT NULL, features text, technician int(11) NOT NULL, solub_date DATE , PRIMARY KEY (id), FOREIGN KEY fk_solub_object(object_id) REFERENCES tbl_object(id), FOREIGN KEY fk_solub_tech(technician) REFERENCES tbl_users(id));
+CREATE TABLE  IF NOT EXISTS tbl_solubility(id int(11) NOT NULL AUTO_INCREMENT, object_id int(11) NOT NULL, description varchar(100) NOT NULL, features text, technician int(11) NOT NULL, solub_date DATE , PRIMARY KEY (id), FOREIGN KEY fk_solub_object(object_id) REFERENCES tbl_object(id), FOREIGN KEY fk_solub_tech(technician) REFERENCES tbl_users(id));
 
 CREATE TABLE IF NOT EXISTS tbl_solubtests(id int(11) NOT NULL AUTO_INCREMENT, tbl_solubilityid int(11) NOT NULL, solvent varchar(150), efficiency int(1), observations varchar(11), PRIMARY KEY (id), FOREIGN KEY fk_solubid(tbl_solubilityid) REFERENCES tbl_solubility(id));
 
-CREATE TABLE IF NOT EXISTS tbl_worksheet(id int(11) NOT NULL AUTO_INCREMENT, object int(11) NOT NULL, worksheet_date DATE NOT NULL, `procedure` varchar(100) NOT NULL, observations varchar(11), materials text, amount int(11), duration int(11), technician int(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY fk_worksheet_object(object_id) REFERENCES tbl_object(id), FOREIGN KEY fk_worksheet_user(technician) REFERENCES tbl_users(id));
+CREATE TABLE IF NOT EXISTS tbl_worksheet(id int(11) NOT NULL AUTO_INCREMENT, object_id int(11) NOT NULL, worksheet_date DATE NOT NULL, procedure_type VARCHAR(100) NOT NULL, observations VARCHAR(11), materials TEXT, amount INT(11), duration INT(11), technician INT(11) NOT NULL, PRIMARY KEY (id), FOREIGN KEY fk_worksheet_object(object_id) REFERENCES tbl_object(id), FOREIGN KEY fk_worksheet_user(technician) REFERENCES tbl_users(id));
