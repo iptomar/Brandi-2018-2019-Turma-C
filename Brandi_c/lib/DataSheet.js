58cdbafde6b89d530f7766b96ef8566a282b4564
Raquel Guedes <aluno19994@ipt.pt>
2019-04-29 05:00:18 +0100

    Desenvolvimento dos webservices de sub categorias, categorias e super categorias
    Desenvolvimento das queries mysql e procedimentos em nodeJs para criar, atualizar, apagar, listar sub categorias, categorias e super categorias
    Desenvolvimento tambem das queries mysql para verificar se uma categoria, super categoria ou sub categira está a ser utilizada num objeto
    Desenvolvimento dos webservices de listar sub categorias, categorias e super categorias
    Falta desenvolver o resto dos webservices criar, atualizar e apagar

--- Brandi_c/lib/DataSheet.js
+++ Brandi_c/lib/DataSheet.js
@@ -3,13 +3,223 @@ const user = require('./User.js');
 const auth = require('./Auth.js');
 const q_DataSheet = require('./queries/q_DataSheet.js');
 
-const ROUTE_USER_PREFIX = "/datasheet";
+const ROUTE_DATASHEET_PREFIX = "/datasheet";
+const ROUTE_CATEGORIES_PREFIX = ROUTE_DATASHEET_PREFIX + "/categories";
+const ROUTE_SUPER_CATEGORIES_PREFIX = ROUTE_DATASHEET_PREFIX + "/super_categories";
+const ROUTE_SUB_CATEGORIES_PREFIX = ROUTE_DATASHEET_PREFIX + "/sub_categories";
+
+/**
+ * lista super categorias
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} search palavra para pesquisar no nome da ficha tecnica
+ * @returns {JSON} {error: <0 se não ocorrer erro, 1 se ocorrer erro>, list:[<objetos>]}
+ */
+async function listSuperCategories(db, search) {
+    let result = { error: 1, list: [] };
+    //listagem de objetos
+    let resultDb = await db.doQuery(q_DataSheet.LIST_SUPER_CATEGORY, ["%" + search + "%"]);
+    //se não ocorreu nenhum erro
+    if (!resultDb.error) {
+        result.error = 0;
+        //devolvemos a lista de fichas tecnicas
+        result.list = resultDb.res;
+    }
+    return result;
+}
+
+
+/**
+ * lista categorias de uma super categoria
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} search palavra para pesquisar no nome da ficha tecnica
+ * @param {number} id_super_category palavra para pesquisar no nome da ficha tecnica
+ * @returns {JSON} {error: <0 se não ocorrer erro, 1 se ocorrer erro>, list:[<objetos>]}
+ */
+async function listCategories(db, id_super_category, search) {
+    let result = { error: 1, list: [] };
+    //listagem de objetos
+    let resultDb = await db.doQuery(q_DataSheet.LIST_CATEGORY, [id_super_category,"%" + search + "%"]);
+    //se não ocorreu nenhum erro
+    if (!resultDb.error) {
+        result.error = 0;
+        //devolvemos a lista de fichas tecnicas
+        result.list = resultDb.res;
+    }
+    return result;
+}
+
+/**
+ * lista sub categorias de uma categoria
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} search palavra para pesquisar no nome da ficha tecnica
+ * @param {number} id_super_category palavra para pesquisar no nome da ficha tecnica
+ * @returns {JSON} {error: <0 se não ocorrer erro, 1 se ocorrer erro>, list:[<objetos>]}
+ */
+async function listSubCategories(db, id_category, search) {
+    let result = { error: 1, list: [] };
+    //listagem de objetos
+    let resultDb = await db.doQuery(q_DataSheet.LIST_SUB_CATEGORY, [id_category,"%" + search + "%"]);
+    //se não ocorreu nenhum erro
+    if (!resultDb.error) {
+        result.error = 0;
+        //devolvemos a lista de fichas tecnicas
+        result.list = resultDb.res;
+    }
+    return result;
+}
+
+
+/**
+ * cria uma nova super categoria
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {string} name nome da super categoria
+ * @returns {number} id da super categoria ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function createSuperCategory(db, name) {
+    let result = -1;
+    //procura super categoria se existe
+    let searchCat = await db.doQuery(q_DataSheet.GET_SUPER_CATEGORY, [name]);
+    if(!searchCat.error) {
+        if(searchCat.res.length === 0) {
+            //criação do novo objeto
+            let resultDb = await db.doQuery(q_DataSheet.CREATE_SUPER_CATEGORY, [name]);
+            //se não ocorreu nenhum erro, devolve o id inserido
+            if (!resultDb.error) {
+                result = resultDb.res.insertId;
+            }
+        }else result = -2;
+    }
+    return result;
+}
+
+/**
+ * cria uma nova categoria
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {string} id_super_category id da supercategoria
+ * @param {string} name nome da categoria
+ * @returns {number} id da categoria ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function createCategory(db, id_super_category,name) {
+    let result = -1;
+    //procura super categoria se existe
+    let searchCat = await db.doQuery(q_DataSheet.GET_CATEGORY, [name]);
+    if(!searchCat.error) {
+        if(searchCat.res.length === 0) {
+            //criação do novo objeto
+            let resultDb = await db.doQuery(q_DataSheet.CREATE_SUPER_CATEGORY, [name,id_super_category]);
+            //se não ocorreu nenhum erro, devolve o id inserido
+            if (!resultDb.error) {
+                result = resultDb.res.insertId;
+            }
+        }else result = -2;
+    }
+    return result;
+}
+
+/**
+ * cria uma nova sub categoria
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {string} id_category id da categoria
+ * @param {string} name nome da sub categoria
+ * @returns {number} id da sub categoria ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function createSubCategory(db, id_category,name) {
+    let result = -1;
+        //procura super categoria se existe
+        let searchCat = await db.doQuery(q_DataSheet.GET_SUB_CATEGORY, [name]);
+        if(!searchCat.error) {
+            if(searchCat.res.length === 0) {
+                //criação do novo objeto
+                let resultDb = await db.doQuery(q_DataSheet.CREATE_SUPER_CATEGORY, [name,id_super_category]);
+                //se não ocorreu nenhum erro, devolve o id inserido
+                if (!resultDb.error) {
+                    result = resultDb.res.insertId;
+                }
+            }else result = -2;
+        }
+    return result;
+}
+
+/**
+ * 
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} id id da supercategoria
+ * @param {string} name nome da supercategoria
+ * @returns {number} 0 se nao ocorrer erro ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function changeSuperCategory(db, id, name) {
+    let result = -1;
+    //procura super categoria se existe
+    let searchCat = await db.doQuery(q_DataSheet.GET_SUPER_CATEGORY, [name]);
+    if(!searchCat.error) {
+        if(searchCat.res.length === 0 || searchCat.res[0].supercategory !== name) {
+            //criação do novo objeto
+            let resultDb = await db.doQuery(q_DataSheet.UPDATE_SUPER_CATEGORY, [name,id]);
+            //se não ocorreu nenhum erro, devolve o id inserido
+            if (!resultDb.error) {
+                result = resultDb.res.affectedRows > 0 ? 0 : -1;
+            }
+        }else result = -2;
+    }
+    return result;
+}
+
+/**
+ * 
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} id id da categoria
+ * @param {string} name nome da categoria
+ * @returns {number} 0 se nao ocorrer erro ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function changeCategory(db, id, name) {
+    let result = -1;
+    //procura super categoria se existe
+    let searchCat = await db.doQuery(q_DataSheet.GET_CATEGORY, [name]);
+    if(!searchCat.error) {
+        if(searchCat.res.length === 0 || searchCat.res[0].category !== name) {
+            //criação do novo objeto
+            let resultDb = await db.doQuery(q_DataSheet.UPDATE_CATEGORY, [name,id]);
+            //se não ocorreu nenhum erro, devolve o id inserido
+            if (!resultDb.error) {
+                result = resultDb.res.affectedRows > 0 ? 0 : -1;
+            }
+        }else result = -2;
+    }
+    return result;
+}
+
+/**
+ * 
+ * @param {database.Database} db Class de ligação à base de dados
+ * @param {number} id id da Subcategoria
+ * @param {string} name nome da Subcategoria
+ * @returns {number} 0 se nao ocorrer erro ou -1 caso ocorra erro, -2 caso o nome ja exista
+ */
+async function changeSubCategory(db, id, name) {
+    let result = -1;
+    //procura super categoria se existe
+    let searchCat = await db.doQuery(q_DataSheet.GET_SUB_CATEGORY, [name]);
+    if(!searchCat.error) {
+        if(searchCat.res.length === 0 || searchCat.res[0].sub_category !== name) {
+            //criação do novo objeto
+            let resultDb = await db.doQuery(q_DataSheet.UPDATE_SUB_CATEGORY, [name,id]);
+            //se não ocorreu nenhum erro, devolve o id inserido
+            if (!resultDb.error) {
+                result = resultDb.res.affectedRows > 0 ? 0 : -1;
+            }
+        } else result = -2;
+    }
+    return result;
+}
+
+//falta os deletes!!!--------------------------------------------------------------------------
+
 
 /**
  * 
  * @param {database.Database} db Class de ligação à base de dados
  * @param {number} search palavra para pesquisar no nome da ficha tecnica
- * @returns {JSON} {error: <0 se não ocorrer erro, 1 se ocorrer erro>, list:[<objetos>]}
+ * @returns {JSON} {error: <0 se não ocorrer erro, 1 se ocorrer erro>, list:[<objects>]}
  */
 async function listDataSheet(db, search) {
     let result = { error: 1, list: [] };
@@ -85,7 +295,7 @@ exports.appendToExpress = function (app, _db, _prefix) {
     let thiss = this;
     let db = _db;
     let prefix = _prefix;
-    app.post(prefix + ROUTE_USER_PREFIX + '/createandedit', async function (req, res) {
+    app.post(prefix + ROUTE_DATASHEET_PREFIX + '/createandedit', async function (req, res) {
         let result = { error: 3, message: "Por favor efectue autenticação", res: {} };
         //verifica se utilizador está autenticado
         let u = auth.getUserFromSession(req);
@@ -128,7 +338,7 @@ exports.appendToExpress = function (app, _db, _prefix) {
         res.json(result);
     });
 
-    app.get(prefix + ROUTE_USER_PREFIX + '/list', async function (req, res) {
+    app.get(prefix + ROUTE_DATASHEET_PREFIX + '/list', async function (req, res) {
         let result = { error: 2, message: "Por favor efectue autenticação", res: { datasheets: [] } };
         //verifica se utilizador está autenticado
         let u = auth.getUserFromSession(req);
@@ -147,4 +357,76 @@ exports.appendToExpress = function (app, _db, _prefix) {
         }
         res.json(result);
     });
+
+    app.get(prefix + ROUTE_CATEGORIES_PREFIX + '/list', async function (req, res) {
+        let result = { error: 3, message: "Por favor efectue autenticação", res: { categories: [] } };
+        //verifica se utilizador está autenticado
+        let u = auth.getUserFromSession(req);
+        if (u) {
+            //caso nao insira o id da categoria
+            result.error = 2;
+            result.message = "O id da super categoria é obrigatório";
+            if(req.query.super_category) {
+                //caso ocorra algum tipo de erro
+                result.error = 1;
+                result.message = "Ocorreu um erro na aquisição da lista, por favor tente novamente";
+                //lista fichas técnicas
+                let resultList = await listCategories(db, req.query.super_category, !req.query.search ? "" : req.query.search);
+                //verifica se ocorreu algum erro
+                if (!resultList.error) {
+                    result.error = 0;
+                    result.message = "Lista de categorias";
+                    result.res.categories = resultList.list;
+                }
+            }
+        }
+        res.json(result);
+    });
+
+    app.get(prefix + ROUTE_SUB_CATEGORIES_PREFIX + '/list', async function (req, res) {
+        let result = { error: 3, message: "Por favor efectue autenticação", res: { sub_categories: [] } };
+        //verifica se utilizador está autenticado
+        let u = auth.getUserFromSession(req);
+        if (u) {
+            //caso nao insira o id da categoria
+            result.error = 2;
+            result.message = "O id da categoria é obrigatório";
+            if(req.query.category) {
+                //caso ocorra algum tipo de erro
+                result.error = 1;
+                result.message = "Ocorreu um erro na aquisição da lista, por favor tente novamente";
+                //lista fichas técnicas
+                let resultList = await listSubCategories(db, req.query.category, !req.query.search ? "" : req.query.search);
+                //verifica se ocorreu algum erro
+                if (!resultList.error) {
+                    result.error = 0;
+                    result.message = "Lista de sub categorias";
+                    result.res.sub_categories = resultList.list;
+                }
+            }
+        }
+        res.json(result);
+    });
+
+    app.get(prefix + ROUTE_SUPER_CATEGORIES_PREFIX + '/list', async function (req, res) {
+        let result = { error: 3, message: "Por favor efectue autenticação", res: { super_categories: [] } };
+        //verifica se utilizador está autenticado
+        let u = auth.getUserFromSession(req);
+        if (u) {
+            //caso ocorra algum tipo de erro
+            result.error = 1;
+            result.message = "Ocorreu um erro na aquisição da lista, por favor tente novamente";
+            //lista fichas técnicas
+            let resultList = await listSuperCategories(db, !req.query.search ? "" : req.query.search);
+            //verifica se ocorreu algum erro
+            if (!resultList.error) {
+                result.error = 0;
+                result.message = "Lista de super categorias";
+                result.res.super_categories = resultList.list;
+            }
+        }
+        res.json(result);
+    });
+
+    //falta webservices de create, change e delete de sub categorias, categorias e super categorias
 };
--- Brandi_c/lib/queries/q_Datasheet.js
+++ Brandi_c/lib/queries/q_Datasheet.js
@@ -5,4 +5,49 @@ exports.CREATE_OBJECT = "INSERT INTO " + infoDB.TBL_OBJECT + "(object_designatio
 //Query DE ATUALIZAÇÃO DE UM OBJETO
 exports.UPDATE_OBJECT = "UPDATE " + infoDB.TBL_OBJECT + " SET object_designation=?, CEARC_process=?, CEARC_process_date=?, CEARC_entry_date=?, LCRM_process=?, LCRM_process_date=?, LCRM_entry_date=?, coordinator=?, last_modified_user=?, last_modified_date=NOW() WHERE id=?";
 //Query DE listagem DE OBJETOS
-exports.LIST_OBJECT = "SELECT a.object_designation, a.CEARC_process, a.CEARC_process_date, a.CEARC_entry_date, a.LCRM_process, a.LCRM_process_date, a.LCRM_entry_date, a.coordinator, b.full_name AS coordinator_name, a.last_modified_user, c.full_name AS last_modified_user_name, a.last_modified_date,a.object_created_date FROM " + infoDB.TBL_OBJECT + " a, " + infoDB.TBL_USERS + " b, " + infoDB.TBL_USERS + " c WHERE a.coordinator=b.id AND a.last_modified_user=c.id AND object_designation like ?";
+exports.LIST_OBJECT = "SELECT a.id,a.object_designation, a.CEARC_process, a.CEARC_process_date, a.CEARC_entry_date, a.LCRM_process, a.LCRM_process_date, a.LCRM_entry_date, a.coordinator, b.full_name AS coordinator_name, a.last_modified_user, c.full_name AS last_modified_user_name, a.last_modified_date,a.object_created_date FROM " + infoDB.TBL_OBJECT + " a, " + infoDB.TBL_USERS + " b, " + infoDB.TBL_USERS + " c WHERE a.coordinator=b.id AND a.last_modified_user=c.id AND object_designation like ?";
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.LIST_SUPER_CATEGORY = "SELECT id,supercategory FROM " + infoDB.TBL_SUPERCATEGORIES + " WHERE supercategory like ?";
+//Query DE listagem DE CATEGORIAS
+exports.LIST_CATEGORY = "SELECT a.id,a.category,b.id as id_super_category FROM " + infoDB.TBL_CATEGORIES + " a, " + infoDB.TBL_SUPERCATEGORIES + " b WHERE a.supercategory=b.id and b.id=? and category like ?";
+//Query DE listagem DE SUB CATEGORIAS
+exports.LIST_SUB_CATEGORY = "SELECT a.id,a.subcategory,b.id as id_category FROM " + infoDB.TBL_SUBCATEGORIES + " a, " + infoDB.TBL_CATEGORIES + " b WHERE a.category=b.id and b.id=? and subcategory like ?";
+
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.GET_SUPER_CATEGORY = "SELECT id,supercategory FROM " + infoDB.TBL_SUPERCATEGORIES + " WHERE supercategory = ? LIMIT 1";
+//Query DE listagem DE CATEGORIAS
+exports.GET_CATEGORY = "SELECT a.id,a.category,b.id as id_super_category FROM " + infoDB.TBL_CATEGORIES + " a, " + infoDB.TBL_SUPERCATEGORIES + " b WHERE a.supercategory=b.id and b.id=? and category = ?  LIMIT 1";
+//Query DE listagem DE SUB CATEGORIAS
+exports.GET_SUB_CATEGORY = "SELECT a.id,a.subcategory,b.id as id_category FROM " + infoDB.TBL_SUBCATEGORIES + " a, " + infoDB.TBL_CATEGORIES + " b WHERE a.category=b.id and b.id=? and subcategory = ?  LIMIT 1";
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.CHECK_IF_USED_SUPER_CATEGORY = "SELECT id FROM " + infoDB.TBL_OBJECT + " WHERE super_category = ? LIMIT 1";
+//Query DE listagem DE CATEGORIAS
+exports.CHECK_IF_USED_CATEGORY = "SELECT id FROM " + infoDB.TBL_OBJECT + " WHERE category = ?  LIMIT 1";
+//Query DE listagem DE SUB CATEGORIAS
+exports.CHECK_IF_USED_SUB_CATEGORY = "SELECT id FROM " + infoDB.TBL_OBJECT + " WHERE sub_category = ?  LIMIT 1";
+
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.CREATE_SUPER_CATEGORY = "INSERT INTO " + infoDB.TBL_SUPERCATEGORIES + "(supercategory) VALUES(?)";
+//Query DE listagem DE CATEGORIAS
+exports.CREATE_CATEGORY = "INSERT INTO " + infoDB.TBL_CATEGORIES + "(category,supercategory) VALUES(?,?)";
+//Query DE listagem DE SUB CATEGORIAS
+exports.CREATE_SUB_CATEGORY = "INSERT INTO " + infoDB.TBL_SUBCATEGORIES + "(subcategory,category) VALUES(?,?)";
+
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.UPDATE_SUPER_CATEGORY = "UPDATE " + infoDB.TBL_SUPERCATEGORIES + " SET supercategory=? WHERE id=?";
+//Query DE listagem DE CATEGORIAS
+exports.UPDATE_CATEGORY = "UPDATE " + infoDB.TBL_CATEGORIES + " SET category=? WHERE id=?";
+//Query DE listagem DE SUB CATEGORIAS
+exports.UPDATE_SUB_CATEGORY = "UPDATE " + infoDB.TBL_SUBCATEGORIES + " SET subcategory=? WHERE id=?";
+
+//Query DE listagem DE SUPER CATEGORIAS
+exports.DELETE_SUPER_CATEGORY = "DELETE FROM " + infoDB.TBL_SUPERCATEGORIES + " WHERE id=?";
+//Query DE listagem DE CATEGORIAS
+exports.DELETE_UPDATE_CATEGORY = "DELETE FROM " + infoDB.TBL_CATEGORIES + " WHERE id=?";
+//Query DE listagem DE SUB CATEGORIAS
+exports.DELETE_UPDATE_SUB_CATEGORY = "DELETE FROM " + infoDB.TBL_SUBCATEGORIES + " WHERE id=?";
