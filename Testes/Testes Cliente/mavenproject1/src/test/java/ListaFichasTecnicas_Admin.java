034ddafe3f516c44a789237f3a821c9e9c14436c
Vanessa Ferreira <aluno19453@ipt.pt>
2019-06-04 07:38:06 +0100

    Tentativa do Utilizador aceder a parte do Admin. (Categorias, Listar Utilizadores...)

--- Testes/Testes Cliente/mavenproject1/src/test/java/ListaFichasTecnicas_Admin.java
+++ Testes/Testes Cliente/mavenproject1/src/test/java/ListaFichasTecnicas_Admin.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -51,14 +52,33 @@ public class ListaFichasTecnicas_Admin {
         driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/div/a[2]")).click();
         Thread.sleep(1000);
         driver.findElement(By.name("searchBox")).sendKeys("teste");
-        
         driver.findElement(By.xpath("/html/body/app-root/div/app-datasheet/div/div/form/div/div/button")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/app-root/div/app-datasheet/div/div/div/div[2]/div/div")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//*[@id=\"datasheet-tabs\"]/li[5]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"datasheet-tabs\"]/li[2]/a")).click();
         Thread.sleep(1000);
-     
+        driver.findElement(By.xpath("/html/body/app-root/div/app-datasheet-edit/form/div[1]/div/div[2]/div/button[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.name("dimensions")).sendKeys("2x3x2");
+        driver.findElement(By.name("other_dimensions")).sendKeys("1x1x1");
+        driver.findElement(By.name("tipology")).sendKeys("teste");
+        driver.findElement(By.name("site")).sendKeys("Local");
+        /*Select prop = new Select (driver.findElement(By.name("owner")));
+        prop.selectByValue("2");
+         Thread.sleep(1000);
+                
+        Select owner = new Select (driver.findElement(By.name("object_owner")));
+        owner.selectByValue("5");
+         Thread.sleep(1000);
+         
+        Select patron = new Select (driver.findElement(By.name("patron")));
+        patron.selectByValue("1");
+         Thread.sleep(1000);
+        */ 
+        
+       // driver.findElement(By.xpath("//*[@id=\"datasheet_part2\"]/app-datasheet-page2/div[5]/div[2]/button")).click();
+        
     }
 
 }
---
+++ Testes/Testes Cliente/mavenproject1/src/test/java/UtilizadorTentarAcederSiteAdmin.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author LittleChinese
+ */
+
+  
+
+public class UtilizadorTentarAcederSiteAdmin {
+     WebDriver driver;
+     
+    @Test
+    public void UtilizadorTentarAcederAdmin() throws Exception {
+        driver = new ChromeDriver();
+
+        driver.get("http://brandic.devll.eu:61080/");
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/button")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//*[@id=\"navbarSupportedContent\"]/a")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.name("email")).sendKeys("teste123456@ipt.pt");
+        driver.findElement(By.name("password")).sendKeys("123456");
+
+        driver.findElement(By.xpath("/html/body/app-root/div/app-login/div/div/form/button")).click();
+        Thread.sleep(1000);
+        
+        
+        //AO TENTAR ACEDER NÃO ACONTECE NADA. CONTINUA NO HOME
+        driver.get("http://brandic.devll.eu:61080/#/admin/user/list");
+        Thread.sleep(1000);
+         //AO TENTAR ACEDER NÃO ACONTECE NADA. CONTINUA NO HOME
+        driver.get("http://brandic.devll.eu:61080/#/admin/user/register");
+        Thread.sleep(1000);
+         //AO TENTAR ACEDER NÃO ACONTECE NADA. CONTINUA NO HOME
+        driver.get("http://brandic.devll.eu:61080/#/admin/categories");
+             
+             
+    }
+    
+}
+    
+    
+
